2a05a2ac4b9ff2a29f8bf9da2a256dfd082eceb9
Odong Sunday <sandieo.2020@gmail.com>
2025-02-24 14:46:48 +0300

    fix layout

--- src/Pages/FileUpload/index.tsx
+++ src/Pages/FileUpload/index.tsx
@@ -1,14 +1,16 @@
-import { Box } from "@mui/material"
+import { Box, Grid } from "@mui/material"
 import { grey } from "@mui/material/colors"
 import { FileUploadButton } from "../../components"
 import DataTable from "../../components/DataGrid"
 
 const FileUpload = () => {
     return (
-        <Box sx={{ p: 6, bgcolor: grey[200], height: "100vh" }}>
-            <FileUploadButton />
+        <Grid container spacing={4} padding={4} mt={4}>
+            <Box sx={{ p: 4, bgcolor: grey[200], width: "100%" }}>
+                <FileUploadButton />
+            </Box>
             <DataTable />
-        </Box>
+        </Grid>
     )
 }
 
